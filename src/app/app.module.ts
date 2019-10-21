804f109691def19e2ed60e2bca74eac60b3339d5
SarwarSaif <saifmu6@gmail.com>
2019-10-21 12:26:59 +0600

    Added Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import { MatInputModule, MatCardModule, MatButtonModule } from '@angular/material';
+import {
+  MatInputModule,
+  MatCardModule,
+  MatButtonModule,
+  MatToolbarModule } from '@angular/material';
 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HeaderComponent} from './header/header.component';
+
 import {PostCreateComponent} from './posts/post-create/post-create.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
     PostCreateComponent
   ],
   imports: [
@@ -21,7 +28,8 @@ import {PostCreateComponent} from './posts/post-create/post-create.component';
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
