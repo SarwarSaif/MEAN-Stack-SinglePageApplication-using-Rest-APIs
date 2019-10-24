2e994df589120b3eaf9593d02a6cb322f93ead66
SarwarSaif <saifmu6@gmail.com>
2019-10-24 16:37:00 +0600

    Fetched Data from the Database

--- backend/app.js
+++ backend/app.js
@@ -42,18 +42,14 @@ app.post('/api/posts',(req, res, next) => {
 });
 
 app.get('/api/posts',(req, res, next) => {
-  const posts = [
-    { id: 'fad12421l',
-      title: 'First server-side post',
-      content: 'This is coming from the server'},
-    { id: 'fad12422l',
-      title: 'Second server-side post',
-      content: 'This is coming from the server!'}
-  ];
-  res.status(200).json({
-    message: 'Posts fetched successfully!',
-    posts: posts
-  });
+  Post.find()
+    .then(documents => {
+      res.status(200).json({
+        message: 'Posts fetched successfully!',
+        posts: documents
+      });
+    });
+
 });
 
 module.exports = app;
