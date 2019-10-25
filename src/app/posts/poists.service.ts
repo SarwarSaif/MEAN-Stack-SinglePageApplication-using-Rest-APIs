fe4e1b1e4e9e122d3c1cdf6c157f9b27c29dc237
SarwarSaif <saifmu6@gmail.com>
2019-10-25 18:04:10 +0600

    Created Edit feature using routing and put method

--- src/app/posts/poists.service.ts
+++ src/app/posts/poists.service.ts
@@ -35,6 +35,10 @@ export class PostsService {
     return this.postsUpdated.asObservable();
   }
 
+  getPost(id: string) {
+    return {...this.posts.find(p => p.id === id)};
+  }
+
   addPost(title: string, content: string) {
     const post: Post = {id: null, title, content};
     this.http.post<{message: string, postId: string}>('http://localhost:3000/api/posts', post)
@@ -45,6 +49,13 @@ export class PostsService {
         this.postsUpdated.next([...this.posts]);
       });
 
+  }
+
+  updatePost(id: string, title: string, content: string) {
+    const post: Post = {id, title, content};
+    this.http
+      .put('http://localhost:3000/api/posts/' + id, post)
+      .subscribe(response => console.log(response));
   }
 
   deletePost(postId: string) {
