bdf1721d9f68e7c362d9e9e8b54d5c5c431a65ed
SarwarSaif <saifmu6@gmail.com>
2019-10-25 08:53:18 +0600

    Added Router Links

--- src/app/posts/poists.service.ts
+++ src/app/posts/poists.service.ts
@@ -52,7 +52,7 @@ export class PostsService {
       'http://localhost:3000/api/posts/' + postId
       )
       .subscribe(() => {
-        const updatedPosts = this.posts.filter(post => post.id!== postId);
+        const updatedPosts = this.posts.filter(post => post.id !== postId);
         this.posts = updatedPosts;
         this.postsUpdated.next([...this.posts]);
         console.log('Deleted');
